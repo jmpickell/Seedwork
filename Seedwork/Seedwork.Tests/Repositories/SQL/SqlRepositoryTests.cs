3612b52470ea01cf980d8e253a6675421dbbe4b1
jmpickell <jmpickell@verizon.net>
2025-07-03 19:25:34 -0500

    Unit tests for sql filter

--- Seedwork/Seedwork.Tests/Repositories/SQL/SqlRepositoryTests.cs
+++ Seedwork/Seedwork.Tests/Repositories/SQL/SqlRepositoryTests.cs
@@ -2,14 +2,18 @@
 using Microsoft.Identity.Client;
 using Moq;
 using Seedwork.Repositories;
+using Seedwork.Repositories.Interfaces;
 using Seedwork.Repositories.SQL;
+using Seedwork.Utilities.Specification.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
 
 namespace Seedwork.Tests.Repositories.SQL
 {
@@ -85,6 +89,22 @@ namespace Seedwork.Tests.Repositories.SQL
             Assert.AreEqual(0, _sut.Value.Read(query, field).GetAll().Count);
         }
 
+        public class MockWrapper<T> : Mock<T>, IMock
+            where T : class
+        {
+            IMock IMock.SetupAllProperties()
+            {
+                var result = base.SetupAllProperties();
+                return this;
+            }
+        }
+
+        public interface IMock 
+        { 
+            object Object { get; }
+            IMock SetupAllProperties();
+        }
+
         [TestMethod]
         public void ReadPK_NoIssues_RowReturned()
         {
@@ -99,6 +119,60 @@ namespace Seedwork.Tests.Repositories.SQL
             var query = new Query<object> { Namespace = "ns", Column = "column", Table = "table", Key = new object() };
 
             Assert.AreEqual(1, _sut.Value.Read(query, "field").GetAll().Count);
+        }
+
+        [TestMethod]
+        [DataRow("ns", "table", ";1=1")]
+        [DataRow(null, "12_34~", "column")]
+        [DataRow("ns", "", "column")]
+        public void ReadFilter_InvalidParameters_NoFieldsReturned(string ns, string table, string column)
+        {
+            var query = new Query<ISpecification<Row>> { Namespace = ns, Column = column, Table = table, Key = new Mock<ISpecification<Row>>().Object };
+            var result = _sut.Value.Read(query, "field");
+            Assert.IsFalse(result.Any());
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ReadFilter_InvalidSpecification_Exception()
+        {
+            var query = new Query<ISpecification<Row>> { Namespace = "ns", Column = "column", Table = "table", Key = new Mock<ISpecification<Row>>().Object };
+            var result = _sut.Value.Read(query, "field").ToArray();
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ReadFilter_InvalidCompositeSpecification_Exception()
+        {
+            var mockSpecification = new Mock<ICompositeSpecification<Row>>();
+            mockSpecification.Setup(x => x.Left).Returns(Filter.Check("F1").LessThan(0));
+            mockSpecification.Setup(x => x.Right).Returns(Filter.Check("F1").LessThan(0));
+
+            var query = new Query<ISpecification<Row>> { Namespace = "ns", Column = "column", Table = "table", Key = mockSpecification.Object };
+            var result = _sut.Value.Read(query, "field").ToArray();
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void ReadFilter_NoIssue_RowsReturned()
+        {
+            var key =
+                Filter.Check("f1").GreaterThan(70) &
+                (Filter.Check("f2").GreaterThan(10000) & !Filter.Check("f4").LessThan(30000)) |
+                Filter.Check("f3").Equals("Europe");
+
+            var mockReader = new Mock<IDataReader>();
+            mockReader.Setup(x => x.FieldCount).Returns(1);
+            mockReader.SetupSequence(x => x.Read()).Returns(true).Returns(true).Returns(false);
+            mockReader.Setup(x => x.GetName(It.IsAny<int>())).Returns("field");
+            mockReader.Setup(x => x.GetValue(It.IsAny<int>())).Returns(new object());
+
+            _mockCommand.Setup(x => x.ExecuteReader()).Returns(mockReader.Object);
+
+            var query = new Query<ISpecification<Row>> { Namespace = "ns", Column = "column", Table = "table", Key = key };
+            var result = _sut.Value.Read(query, "field");
+            foreach (var row in result)
+                Assert.AreEqual(1, row.GetAll().Count);
         }
         #endregion
 
